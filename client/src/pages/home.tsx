402fda7faa670f8a63dde54c0d54fb4c2fcf105f
Miralhas <victor2.miralhas@gmail.com>
2026-01-17 16:58:21 -0300

    fix: change wrong application name

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
   return (
     <Container className="p-6 space-y-1 font-roboto relative overflow-hidden md:overflow-visible">
       <h1 className="font-tilt text-center text-2xl uppercase tracking-widest bg-linear-to-r from-red-500 via-primary/40 to-primary/10 bg-clip-text text-transparent font-bold mb-6">
-        LGMC - Samples</h1>
+        LGCM - Samples</h1>
       <SampleForm />
       <div className="space-y-4 md:space-y-0 md:grid md:grid-cols-2 grid-rows-[min-content_400px] gap-y-1 gap-x-4 p-3 relative z-50">
         <SamplesTable />
